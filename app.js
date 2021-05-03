329781277d67326c038e3877eeba96fd6434430d
SarkerShahriar <anindoshahriar97@gmail.com>
2021-05-04 00:53:48 +0600

    init

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-    console.log('hello from the middleware');
-    next();
-})
+ 
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -25,10 +21,32 @@ app.use((req, res, next) => {
 })
 
 
+
 // 3) ROUTES 
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) SERVER
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't fin ${req.originalUrl} on this server!`
+    // })
+
+    const err = new Error(`Can't fin ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
 
 module.exports = app;
